82e5c8fce85ad7722f02d7a7e2c426a67635cc31
rounakbera <rounakbera@gmail.com>
2020-05-17 18:02:17 -0500

    hscroll

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styled from 'styled-components';
 import Title from './components/Title.js';
 import Images from './components/Images.js';
@@ -11,21 +11,39 @@ const AppWrapper = styled.div`
 	background-color: #00071A;
 	display: flex;
 	overflow-y: hidden;
-`
+` 
 
-function App() {
-  return (
-	<>
-		<style>
-			@import url('https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400&family=Open+Sans:ital,wght@0,400;0,600;1,400&display=swap');
-		</style>
-    	<AppWrapper>
-    		<Title/>
-			<Images/>
-			<Words/>
-		</AppWrapper>
-	</>
-  );
+class App extends Component {
+	constructor(props){
+		super(props)
+		this.hScroll.bind(this)
+	}
+
+	hScroll = (e) => {
+		// e.preventDefault()
+		var container = document.getElementById('container')
+		var containerScrollPosition = document.getElementById('container').scrollLeft
+		container.scrollTo({
+			top: 0,
+			left: containerScrollPosition + e.deltaY,
+			behaviour: 'smooth' //if you want smooth scrolling
+		})
+	}
+
+	render(){
+		return (
+			<div id='container' onWheel={this.hScroll} style={{width: '100vw',height: '100vh',overflow:'scroll'}}>
+				<style>
+					@import url('https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400&family=Open+Sans:ital,wght@0,400;0,600;1,400&display=swap');
+				</style>
+				<AppWrapper>
+					<Title/>
+					<Images/>
+					<Words/>
+				</AppWrapper>
+			</div>
+		)
+	}
 }
 
 export default App;
